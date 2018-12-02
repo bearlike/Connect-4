e53ff2c3bc9c18fef2693a7a73be29a143ae3206
Krishnakanth Alagiri <39209037+bearlike@users.noreply.github.com>
2018-12-03 02:09:56 +0530

    Create Config area

--- Connect4_Game.py
+++ Connect4_Game.py
@@ -1,5 +1,30 @@
+# ==============================================================================
+# Made by Krishna Alagiri, https://github.com/KrishnaAlagiri                   |
+# Started on Decemeber 11:00 PM, 03/12/2018                                              |
+# ==============================================================================
 import random
 import copy
 import sys
 import pygame
 from pygame.locals import *
+
+""" Configuration Area """
+
+board_width = 4 # Width of the Board
+board_height = 6 # Height of the Board
+space = 50 # Size of the each tokens and individual board spaces (in pixels)
+fps = 30 # frames per second to update the screen
+window_width = 640 # width of the program's window, in pixels
+window_height = 480 # height in pixels
+bgcolour = (0,0,0) # Background colour default set to BLACK
+text colour = (225,225,225) # Text colour default set to WHITE
+X_margin = int((window_width - board_width * space) / 2) # Calculate X-Margin
+Y_margin = int((window_height - board_height * space) / 2) # Calculate Y-Margin
+
+""" End of Configuration Area """
+
+main():
+    pass
+
+if __name__ == '__main__':
+    main()
