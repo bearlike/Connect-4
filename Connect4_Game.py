c78979cbc8ca9bd958d98baae160d997d1e5c761
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2018-12-03 02:29:10 +0530

    fix

--- Connect4_Game.py
+++ Connect4_Game.py
@@ -17,7 +17,7 @@ FPS = 30 # frames per second to update the screen
 window_width = 640 # width of the program's window, in pixels
 window_height = 480 # height in pixels
 bgcolour = (0,0,0) # Background colour default set to BLACK
-text colour = (225,225,225) # Text colour default set to WHITE
+text_colour = (225,225,225) # Text colour default set to WHITE
 X_margin = int((window_width - board_width * space) / 2) # Calculate X-Margin
 Y_margin = int((window_height - board_height * space) / 2) # Calculate Y-Margin
 
