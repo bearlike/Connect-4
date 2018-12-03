c91bd2aedf60c77b2a5d155438a2e0ae5da3b9ac
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2018-12-03 11:18:09 +0530

    update Functions

--- Connect4_Game.py
+++ Connect4_Game.py
@@ -23,9 +23,45 @@ X_margin = int((window_width - board_width * space) / 2) # Calculate X-Margin
 Y_margin = int((window_height - board_height * space) / 2) # Calculate Y-Margin
 p1 = 'Player 1'
 p2 = 'Player 2'
+red = 'red'
+yellow = 'yellow'
+EMPTY = None
 """ End of Configuration Area """
+""" Functions Begin """
+
+def DrawBoard(board, extraToken=None):
+    screen.fill(bgcolour)
+    pass
 
 
+def runGAME():
+    turn = p1
+    # Set up a blank board data structure.
+    mainBoard = resetBoard()
+    while True: # main game loop
+        # Player 1
+        if turn == p1:
+            P1_getMove(mainBoard)
+            if isWinner(mainBoard, red):
+                break
+            turn = p2
+        # Player 2
+        else:
+            P2_getMove(mainBoard)
+            if isWinner(mainBoard, yellow):
+                break
+            turn = p1
+        # Tie
+        if isBoardFull(mainBoard):
+            breaks
+    DrawBoard(mainBoard)
+    pass
+
+def resetBoard():
+    board = []
+    for x in range(0, board_width):
+        board.append([empty]*board_height)
+    return board
 
 def main():
     if board_width < 4 and board_height < 4:
@@ -33,38 +69,15 @@ def main():
         exit()
 
     global clock, screen, redpilerect, yellowpilerect, redtoken_img
-    global yellowtoken_img, board_img, arrow_img, arrowrect, p1w_img
+    global yellowtoken_img, board_img, p1w_img
     global p2w_img, winnerrect, tie_img
 
     pygame.init()
     clock = pygame.time.Clock()
     screen = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption('Connect 4: Created by Krishna Alagiri')
-    screen.fill(bgcolour)
-    redpilerect = pygame.Rect(int(space/2), window_height-int(3*space/2), space, space)
-    yellowpilerect = pygame.Rect(window_width-int(3*space/2), window_height-int(3*space/2), space, space)
-    redtoken_img = pygame.image.load('red_coin.png')
-    redtoken_img = pygame.transform.smoothscale(redtoken_img, (space, space))
-    yellowtoken_img = pygame.image.load('yellow_coin.png')
-    yellowtoken_img = pygame.transform.smoothscale(yellowtoken_img, (space, space))
-    board_img = pygame.image.load('board_piece.png')
-    board_img = pygame.transform.smoothscale(board_img, (space, space))
 
     runGAME()
-
-def runGAME():
-    turn = p1
-    # Set up a blank board data structure.
-    mainBoard = resetBoard()
-    pass
-
-def resetBoard():
-    board = []
-    for x in range(board_width):
-        board.append([EMPTY] * board_height)
-    return board
-
-
 
 if __name__ == '__main__':
     main()
